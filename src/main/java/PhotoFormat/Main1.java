99728eae0ae9c014c28a97bdd054b1123521e2c8
Stanislaw <sta.glegola@gmail.com>
2019-07-26 14:03:07 +0200

    PhotoFormatFactory working

---
+++ src/main/java/PhotoFormat/Main1.java
@@ -0,0 +1,56 @@
+package PhotoFormat;
+
+interface PhotoViever {
+    void showPhoto(String fileName);
+}
+
+class JPGformat implements PhotoViever {
+
+    public void showPhoto(String fileName) {
+        System.out.println("Show photograph in .JPG format: " + fileName);
+    }
+}
+
+class JPEGformat implements PhotoViever {
+
+    public void showPhoto(String fileName) {
+        System.out.println("Show photograph in .JPEG format: " + fileName);
+    }
+}
+
+class PNGformat implements PhotoViever {
+
+    public void showPhoto(String fileName) {
+        System.out.println("Show photograph in .JPNG format: " + fileName);
+    }
+}
+
+class FormatReaderFactory {
+    public PhotoViever reader(String fileName) {
+        if (fileName
+                .endsWith(".JPG")) {
+            return new JPGformat();
+        } else if (fileName
+                .endsWith(".JPEG")) {
+            return new JPEGformat();
+        } else if (fileName
+                .endsWith(".PNG")) {
+            return new PNGformat();
+        } else {
+            return null;
+        }
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        String fileName = "photo.JPG";
+
+        FormatReaderFactory formatReader = new FormatReaderFactory();
+
+        PhotoViever reader = formatReader.reader(fileName);
+
+        reader.showPhoto(fileName);
+    }
+}
