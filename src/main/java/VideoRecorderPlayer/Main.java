3e8d5c07d8b53774b9a848fe650eb42e6960059b
Stanislaw <sta.glegola@gmail.com>
2019-07-26 14:04:01 +0200

    VideoRecorderFactory working

---
+++ src/main/java/VideoRecorderPlayer/Main.java
@@ -0,0 +1,63 @@
+package VideoRecorderPlayer;
+
+interface VideoRecorder {
+    void play(String fileName);
+}
+
+class MP4Player implements VideoRecorder {
+
+    public void play(String fileName) {
+        System.out.println("Play mp4 extention: " + fileName);
+    }
+}
+
+class AVIPlayer implements VideoRecorder {
+
+    public void play(String fileName) {
+        System.out.println("Play avi extention: " + fileName);
+    }
+}
+
+class PlayerFactory {
+    public VideoRecorder player(String filepath){
+        if (filepath.endsWith(".avi")){
+            return new AVIPlayer();
+        }
+        else if (filepath.endsWith(".mp4")){
+            return new MP4Player();
+        }
+        else {
+            return null;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String fileName = "sample.mp4";
+
+        PlayerFactory factory = new PlayerFactory();
+
+        VideoRecorder player = factory.player(fileName);
+
+        player.play(fileName);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
